8acb26e0bb0d6001af24ed83aefbed6b9321dffc
manit <ermanit007@gmail.com>
2017-09-25 10:20:11 +0530

    tabhost button design a slide activity

---
+++ app/src/main/java/com/grademojo/fragmentactivity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.grademojo.fragmentactivity;
+
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+
+    FragmentManager fm;
+    FragmentTab fragmentTab = new FragmentTab();
+    private FragmentTabHost mTabHost;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab1").setIndicator("Tab 1", null),
+                FragmentTab.class, null);
+
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab2").setIndicator("Tab 2", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab3").setIndicator("Tab 3", null),
+                FragmentTab.class, null);
+    }
+}
